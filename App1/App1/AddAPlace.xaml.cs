90211792037620c8b6448c2fdf88f7a30773fe70
reiniskalbergs <r.kalbergs@gmail.com>
2016-08-24 17:44:38 +0300

    first commit

---
+++ App1/App1/AddAPlace.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class AddAPlace : ContentPage
+    {
+        public AddAPlace()
+        {
+            InitializeComponent();
+            private async void ToAddAPlace(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddAPlace());
+        }
+    }
+}
---
+++ App1/App1/SecondPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class SecondPage : ContentPage
+    {
+        public SecondPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ToAddAPlacw(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddAPlace());
+        }
+    }
+}
